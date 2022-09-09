81bebfa454121589bc4c6dd8c7b1f9a2ca2e3eef
john kaiser <52598487+johnkaizer@users.noreply.github.com>
2022-09-09 21:31:51 +0300

    First commit

---
+++ app/src/main/java/com/project/mkulima/WelcomeActivity.kt
@@ -0,0 +1,25 @@
+package com.project.mkulima
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+        var Sign= findViewById<TextView>(R.id.login)
+        var login= findViewById<TextView>(R.id.signup)
+
+        Sign.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+        login.setOnClickListener{
+            val intent = Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
